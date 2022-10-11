15611cb65b1933af11c3810891c1462dde9ccfec
andrewtioo <andrew.prasetyo@wahyoo.com>
2022-10-11 20:38:21 +0700

    creating users part 4-7

--- models/note.js
+++ models/note.js
@@ -11,6 +11,10 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 // Change _id(Object) to id(String)
